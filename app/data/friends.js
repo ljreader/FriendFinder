6c5e39e322f5fec833935432c68cfefa5e10ef59
ljreader <l.j.reader@outlook.com>
2018-01-03 20:07:55 -0700

    basic layouts

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+    {
+        "name":"Ryan",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+]
+  
+  module.exports = friendList;
