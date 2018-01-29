bb6246fd314f6c907046fc0eff87a732c04235c1
ljreader <lawrencereader@outlook.com>
2018-01-29 15:40:15 -0700

    changed values

--- app/data/friends.js
+++ app/data/friends.js
@@ -20,151 +20,155 @@ var friendList = [
     },
     // Bob
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name":"Bob",
+        "photo":"https://1drv.ms/i/s!AneOheT2fviYhNgtNSUvQfJweBug8A",
         "scores":[
-            5,
-            1,
+            4,
+            3,
             4,
             4,
             5,
             1,
-            2,
+            5,
             5,
             4,
             1
         ]
-    },        
+    },      
+    // Mike  
     {
-        "name":"Steve",
-        "photo":"https://1drv.ms/i/s!AneOheT2fviYhNUqauBrDS7DaXPYxw",
+        "name":"Mike",
+        "photo":"https://1drv.ms/i/s!AneOheT2fviYhNgxm_aDwwT0gghwRQ",
         "scores":[
             5,
             1,
             4,
+            2,
+            2,
+            1,
+            2,
+            2,
+            4,
+            1
+        ]
+    },
+    // John
+    {
+        "name":"John",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            3,
+            4,
+            4,
             4,
             5,
             1,
-            2,
+            1,
+            5,
+            4,
+            1
+        ]
+    },     
+    // Gary   
+    {
+        "name":"Gary",
+        "photo":"https://1drv.ms/i/s!AneOheT2fviYhNgs6ydnbni8C0aglg",
+        "scores":[
+            4,
+            1,
+            4,
+            3,
+            5,
+            1,
+            3,
             5,
             4,
             1
         ]
     },
-    // Bob
+    // Doug
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name":"Doug",
+        "photo":"https://1drv.ms/i/s!AneOheT2fviYhNgyAqzivPVwEQgcTA",
         "scores":[
             5,
-            1,
+            5,
             4,
             4,
             5,
             1,
-            2,
+            5,
             5,
             4,
             1
         ]
-    },        
+    },      
+    // Erik  
     {
-        "name":"Steve",
-        "photo":"https://1drv.ms/i/s!AneOheT2fviYhNUqauBrDS7DaXPYxw",
+        "name":"Erik",
+        "photo":"https://1drv.ms/i/s!AneOheT2fviYhNg1hikHApk0Ftneug",
         "scores":[
             5,
-            1,
+            4,
             4,
             4,
             5,
-            1,
+            4,
             2,
             5,
             4,
+            4
+        ]
+    },
+    // Ryan
+    {
+        "name":"Ryan",
+        "photo":"https://1drv.ms/i/s!AneOheT2fviYhNg6Wo8cNy5DaSS2xA",
+        "scores":[
+            1,
+            1,
+            4,
+            4,
+            1,
+            1,
+            1,
+            5,
+            1,
+            1
+        ]
+    },      
+    // Nate  
+    {
+        "name":"Nate",
+        "photo":"https://1drv.ms/i/s!AneOheT2fviYhNg5bjxXCJgiIuKlQw",
+        "scores":[
+            5,
+            1,
+            3,
+            4,
+            5,
+            3,
+            2,
+            5,
+            3,
             1
         ]
     },
-    // Bob
+    // Jacob
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name":"Jacob",
+        "photo":"https://1drv.ms/i/s!AneOheT2fviYhNg4nej6ZSTgOyOTfg",
         "scores":[
-            5,
+            3,
             1,
             4,
-            4,
+            3,
             5,
             1,
-            2,
+            3,
             5,
             4,
-            1
-        ]
-    },        
-    {
-        "name":"Steve",
-        "photo":"https://1drv.ms/i/s!AneOheT2fviYhNUqauBrDS7DaXPYxw",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },
-    // Bob
-    {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },        
-    {
-        "name":"Steve",
-        "photo":"https://1drv.ms/i/s!AneOheT2fviYhNUqauBrDS7DaXPYxw",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },
-    // Bob
-    {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
+            3
         ]
     },
 ]        
--- homework_instructions.md
+++ homework_instructions.md
@@ -45,10 +45,10 @@ You will use Express to handle routing. Make sure you deploy your app to Heroku 
    <!-- * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends. -->
    <!-- * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.  -->
 
-5. You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
+<!-- 5. You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below. -->
 
-```json
-{
+<!-- ```json -->
+<!-- {
   "name":"Ahmed",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
   "scores":[
@@ -64,7 +64,7 @@ You will use Express to handle routing. Make sure you deploy your app to Heroku 
       1
     ]
 }
-```
+``` -->
 
 6. Determine the user's most compatible friend using the following as a guide:
 
